ebd18700ed8a994faee72c609ffc3dfc186391ac
timschneider <tim-3.schneider@uni-ulm.de>
2022-01-02 11:46:09 +0100

    reformat files

--- GAN/generator_output.py
+++ GAN/generator_output.py
@@ -5,6 +5,7 @@ import PIL
 import PIL.Image
 import pathlib
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import argparse
@@ -13,51 +14,52 @@ import argparse
 
 img_height = 72
 img_width = 128
-image_size = (72,128,3)
+image_size = (72, 128, 3)
 z_size = 128
 
-def output_results(batch_size, checkpoints, epochs, every, output_image):
 
+def output_results(batch_size, checkpoints, epochs, every, output_image):
     if tf.test.is_gpu_available():
-      device_name = '/GPU:0'
+        device_name = '/GPU:0'
     else:
-      device_name = '/CPU:0'
-    
+        device_name = '/CPU:0'
+
     print(device_name)
-    
+
     gen_model = tf.keras.models.load_model(checkpoints + "/gen-model")
     gen_model.summary()
-    
+
     gen_checkpoint_path = checkpoints + "/gen-{epoch:04d}.ckpt"
     gen_checkpoint_dir = os.path.dirname(gen_checkpoint_path)
-    
+
     def create_samples(g_model, input_z):
-      g_output = g_model(input_z, training=False)
-      images = tf.reshape(g_output, (batch_size, *image_size))
-      return (images+1)/2.0
-    
+        g_output = g_model(input_z, training=False)
+        images = tf.reshape(g_output, (batch_size, *image_size))
+        return (images + 1) / 2.0
+
     epoch_samples = []
 
     fixed_z = tf.random.uniform(shape=(batch_size, z_size), minval=-1, maxval=1)
-    
+
     for i in range(epochs):
         if i % every == 0:
             checkpoint = gen_checkpoint_path.format(epoch=i)
             gen_model.load_weights(checkpoint)
-            
-            epoch_samples.append(create_samples(gen_model, fixed_z).numpy())
-    
-    de_normalization_layer = tf.keras.layers.Rescaling(1./2., offset=0.5)
 
-    fig = plt.figure(figsize=(10,14))
+            epoch_samples.append(create_samples(gen_model, fixed_z).numpy())
+
+    de_normalization_layer = tf.keras.layers.Rescaling(1. / 2., offset=0.5)
+
+    fig = plt.figure(figsize=(10, 14))
     for i in range(epochs):
         if i % every == 0:
             for j in range(3):
-              ax = fig.add_subplot(epochs // every, 3, i*3+j+1)
-              image = epoch_samples[i][j]
-              image = de_normalization_layer(image)
-              ax.imshow(image)
+                ax = fig.add_subplot(epochs // every, 3, i * 3 + j + 1)
+                image = epoch_samples[i][j]
+                image = de_normalization_layer(image)
+                ax.imshow(image)
     fig.savefig(output_image)
+
 
 if __name__ == '__main__':
     # Parse Arguments #
@@ -65,8 +67,10 @@ if __name__ == '__main__':
     parser.add_argument('bSize', type=int, help='Batch Size to use')
     parser.add_argument('epochs', type=int, help='Epochs available')
     parser.add_argument('every', type=int, help='Pint example every x epochs')
-    parser.add_argument('-c', '--checkpoints', type=str, dest="checkpoints", default="training", help="The output directory where the checkpoints are saved.")
-    parser.add_argument('-o', '--output', type=str, dest="output", default="training", help="The name of the image to (over-)write")
+    parser.add_argument('-c', '--checkpoints', type=str, dest="checkpoints", default="training",
+                        help="The output directory where the checkpoints are saved.")
+    parser.add_argument('-o', '--output', type=str, dest="output", default="training",
+                        help="The name of the image to (over-)write")
 
     args = parser.parse_args()
     output_results(args.bSize, args.checkpoints, args.epochs, args.every, args.output)
--- GAN/generator_train.py
+++ GAN/generator_train.py
@@ -5,6 +5,7 @@ import PIL
 import PIL.Image
 import pathlib
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import argparse
@@ -17,138 +18,141 @@ np.random.seed(1)
 
 img_height = 72
 img_width = 128
-image_size = (72,128,3)
+image_size = (72, 128, 3)
 z_size = 128
+
 
 # Generator #
 
-def make_dcgan_generator(output_size=(72,128,3)):
-  n_filters = 512
-  hidden_size = (output_size[0]//8, output_size[1]//8)
+def make_dcgan_generator(output_size=(72, 128, 3)):
+    n_filters = 512
+    hidden_size = (output_size[0] // 8, output_size[1] // 8)
 
-  model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=(128,)),
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(128,)),
 
-    tf.keras.layers.Dense(units=512*np.prod(hidden_size), use_bias=False),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.Reshape((hidden_size[0], hidden_size[1], 512)),
+        tf.keras.layers.Dense(units=512 * np.prod(hidden_size), use_bias=False),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Reshape((hidden_size[0], hidden_size[1], 512)),
 
-    tf.keras.layers.Conv2DTranspose(
-        filters=256, kernel_size=(4,4),
-        strides=(2,2), padding='same', use_bias=False
-    ),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
+        tf.keras.layers.Conv2DTranspose(
+            filters=256, kernel_size=(4, 4),
+            strides=(2, 2), padding='same', use_bias=False
+        ),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
 
-    tf.keras.layers.Conv2DTranspose(
-        filters=128, kernel_size=(4,4),
-        strides=(2,2), padding='same', use_bias=False
-    ),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
+        tf.keras.layers.Conv2DTranspose(
+            filters=128, kernel_size=(4, 4),
+            strides=(2, 2), padding='same', use_bias=False
+        ),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
 
-    tf.keras.layers.Conv2DTranspose(
-        filters=64, kernel_size=(4,4),
-        strides=(2,2), padding='same', use_bias=False
-    ),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.ReLU(),
+        tf.keras.layers.Conv2DTranspose(
+            filters=64, kernel_size=(4, 4),
+            strides=(2, 2), padding='same', use_bias=False
+        ),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
 
-    tf.keras.layers.Conv2DTranspose(
-          filters=3, kernel_size=(3,3),
-          strides=(1,1), padding='same', use_bias=False,
-          activation='tanh'
-      )
-  ])
+        tf.keras.layers.Conv2DTranspose(
+            filters=3, kernel_size=(3, 3),
+            strides=(1, 1), padding='same', use_bias=False,
+            activation='tanh'
+        )
+    ])
 
-  return model
+    return model
+
 
 # Discriminator #
 
-def make_dcgan_discriminator(dropout_rate, input_size=(72,128,3)):
-  model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=input_size),
+def make_dcgan_discriminator(dropout_rate, input_size=(72, 128, 3)):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=input_size),
 
-    tf.keras.layers.Conv2D(
-        filters=64, kernel_size=(3,3),
-        strides=(1,1), padding='same'
-    ),
-    tf.keras.layers.LeakyReLU(alpha=0.1),
-    tf.keras.layers.Dropout(dropout_rate),
+        tf.keras.layers.Conv2D(
+            filters=64, kernel_size=(3, 3),
+            strides=(1, 1), padding='same'
+        ),
+        tf.keras.layers.LeakyReLU(alpha=0.1),
+        tf.keras.layers.Dropout(dropout_rate),
 
-    tf.keras.layers.Conv2D(
-        filters=128, kernel_size=(4,4),
-        strides=(2,2), padding='same'
-    ),
-    tf.keras.layers.LeakyReLU(alpha=0.1),
-    tf.keras.layers.Conv2D(
-        filters=128, kernel_size=(3,3),
-        strides=(1,1), padding='same'
-    ),
-    tf.keras.layers.LeakyReLU(alpha=0.1),
-    tf.keras.layers.Dropout(dropout_rate),
+        tf.keras.layers.Conv2D(
+            filters=128, kernel_size=(4, 4),
+            strides=(2, 2), padding='same'
+        ),
+        tf.keras.layers.LeakyReLU(alpha=0.1),
+        tf.keras.layers.Conv2D(
+            filters=128, kernel_size=(3, 3),
+            strides=(1, 1), padding='same'
+        ),
+        tf.keras.layers.LeakyReLU(alpha=0.1),
+        tf.keras.layers.Dropout(dropout_rate),
 
-    tf.keras.layers.Conv2D(
-        filters=256, kernel_size=(4,4),
-        strides=(2,2), padding='same'
-    ),
-    tf.keras.layers.LeakyReLU(alpha=0.1),
-    tf.keras.layers.Dropout(dropout_rate),
+        tf.keras.layers.Conv2D(
+            filters=256, kernel_size=(4, 4),
+            strides=(2, 2), padding='same'
+        ),
+        tf.keras.layers.LeakyReLU(alpha=0.1),
+        tf.keras.layers.Dropout(dropout_rate),
 
-    tf.keras.layers.Conv2D(
-        filters=256, kernel_size=(3,3),
-        strides=(1,1), padding='same'
-    ),
-    tf.keras.layers.LeakyReLU(alpha=0.1),
-    tf.keras.layers.Dropout(dropout_rate),
+        tf.keras.layers.Conv2D(
+            filters=256, kernel_size=(3, 3),
+            strides=(1, 1), padding='same'
+        ),
+        tf.keras.layers.LeakyReLU(alpha=0.1),
+        tf.keras.layers.Dropout(dropout_rate),
 
-    tf.keras.layers.Conv2D(
-        filters=512, kernel_size=(4,4),
-        strides=(2,2), padding='same'
-    ),
-    tf.keras.layers.LeakyReLU(alpha=0.1),
-    tf.keras.layers.Dropout(dropout_rate),
+        tf.keras.layers.Conv2D(
+            filters=512, kernel_size=(4, 4),
+            strides=(2, 2), padding='same'
+        ),
+        tf.keras.layers.LeakyReLU(alpha=0.1),
+        tf.keras.layers.Dropout(dropout_rate),
 
-    tf.keras.layers.Conv2D(
-        filters=512, kernel_size=(3,3),
-        strides=(1,1), padding='same'
-    ),
-    tf.keras.layers.LeakyReLU(alpha=0.1),
-    tf.keras.layers.Dropout(dropout_rate),
+        tf.keras.layers.Conv2D(
+            filters=512, kernel_size=(3, 3),
+            strides=(1, 1), padding='same'
+        ),
+        tf.keras.layers.LeakyReLU(alpha=0.1),
+        tf.keras.layers.Dropout(dropout_rate),
 
-    tf.keras.layers.Conv2D(
-        filters=1, kernel_size=(9,16)
-    ),
+        tf.keras.layers.Conv2D(
+            filters=1, kernel_size=(9, 16)
+        ),
 
-    tf.keras.layers.Reshape((1,))
-  ])
+        tf.keras.layers.Reshape((1,))
+    ])
 
-  return model
+    return model
+
 
 def get_dataset(data, batch_size):
     data_dir = pathlib.Path(data)
-    
+
     train_ds = tf.keras.utils.image_dataset_from_directory(
-      data_dir,
-      seed=123,
-      image_size=(img_height,img_width),
-      batch_size=batch_size,
-      crop_to_aspect_ratio=True)
-    
-    normalization_layer = tf.keras.layers.Rescaling(1./127.5, offset=-1)
+        data_dir,
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size,
+        crop_to_aspect_ratio=True)
+
+    normalization_layer = tf.keras.layers.Rescaling(1. / 127.5, offset=-1)
     train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
     train_ds = train_ds.shuffle(60).cache()
     return train_ds
-    
+
 
 def train_models(checkpoints, data, checkpoint_frequency, batch_size, num_epochs, dropout, learning_rate):
     # Check GPU #
     if len(tf.config.list_physical_devices('GPU')) != 0:
-      #device_name = tf.config.list_physical_devices('GPU')[0].name
-      device_name = '/GPU:0'
+        # device_name = tf.config.list_physical_devices('GPU')[0].name
+        device_name = '/GPU:0'
     else:
-      device_name = '/CPU:0'
+        device_name = '/CPU:0'
 
     print(device_name)
 
@@ -157,12 +161,12 @@ def train_models(checkpoints, data, checkpoint_frequency, batch_size, num_epochs
     print("###################################")
     print("Using Generator-Model:")
     gen_model.summary()
-    
+
     gen_checkpoint_path = checkpoints + "/gen-{epoch:04d}.ckpt"
     gen_checkpoint_dir = os.path.dirname(gen_checkpoint_path)
 
     gen_model.save(gen_checkpoint_dir + "/gen-model")
-    
+
     # Create Discriminator #
     disc_model = make_dcgan_discriminator(dropout)
     print("###################################")
@@ -171,68 +175,68 @@ def train_models(checkpoints, data, checkpoint_frequency, batch_size, num_epochs
 
     disc_checkpoint_path = checkpoints + "/disc-{epoch:04d}.ckpt"
     disc_checkpoint_dir = os.path.dirname(disc_checkpoint_path)
-    
+
     disc_model.save(disc_checkpoint_dir + "/disc-model")
-    
+
     train_ds = get_dataset(data, batch_size)
-    
+
     # Training #
-    
+
     loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     g_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     d_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-    
+
     start_time = time.time()
-    
+
     for epoch in range(num_epochs):
-    
-      epoch_losses, epoch_d_vals = [], []
-    
-      for input_real, _ in train_ds:
-            input_z = tf.random.uniform(shape=(batch_size,z_size), minval=-1.0, maxval=1.0)
+
+        epoch_losses, epoch_d_vals = [], []
+
+        for input_real, _ in train_ds:
+            input_z = tf.random.uniform(shape=(batch_size, z_size), minval=-1.0, maxval=1.0)
             with tf.GradientTape() as g_tape:
-              g_output = gen_model(input_z)
-              d_logits_fake = disc_model(g_output, training=True)
-              labels_real = tf.ones_like(d_logits_fake)
-              g_loss = loss_fn(y_true=labels_real,y_pred=d_logits_fake)
-            
+                g_output = gen_model(input_z)
+                d_logits_fake = disc_model(g_output, training=True)
+                labels_real = tf.ones_like(d_logits_fake)
+                g_loss = loss_fn(y_true=labels_real, y_pred=d_logits_fake)
+
             g_grads = g_tape.gradient(g_loss, gen_model.trainable_variables)
-            g_optimizer.apply_gradients(grads_and_vars=zip(g_grads,gen_model.trainable_variables))
-            
+            g_optimizer.apply_gradients(grads_and_vars=zip(g_grads, gen_model.trainable_variables))
+
             with tf.GradientTape() as d_tape:
-              d_logits_real = disc_model(input_real,training=True)
-              d_labels_real = tf.ones_like(d_logits_real)
-              d_loss_real = loss_fn(y_true=d_labels_real,y_pred=d_logits_real)
-              
-              d_logits_fake = disc_model(g_output,training=True)
-              d_labels_fake = tf.zeros_like(d_logits_fake)
-              d_loss_fake = loss_fn(y_true=d_labels_fake,y_pred=d_logits_fake)
-            
-              d_loss = d_loss_real + d_loss_fake
-            
-            if d_loss > 0.001: # To fix loss of dicriminator getting too low
-                d_grads = d_tape.gradient(d_loss,disc_model.trainable_variables)
-                d_optimizer.apply_gradients(grads_and_vars=zip(d_grads,disc_model.trainable_variables))
-            
+                d_logits_real = disc_model(input_real, training=True)
+                d_labels_real = tf.ones_like(d_logits_real)
+                d_loss_real = loss_fn(y_true=d_labels_real, y_pred=d_logits_real)
+
+                d_logits_fake = disc_model(g_output, training=True)
+                d_labels_fake = tf.zeros_like(d_logits_fake)
+                d_loss_fake = loss_fn(y_true=d_labels_fake, y_pred=d_logits_fake)
+
+                d_loss = d_loss_real + d_loss_fake
+
+            if d_loss > 0.001:  # To fix loss of dicriminator getting too low
+                d_grads = d_tape.gradient(d_loss, disc_model.trainable_variables)
+                d_optimizer.apply_gradients(grads_and_vars=zip(d_grads, disc_model.trainable_variables))
+
             epoch_losses.append(
                 (g_loss.numpy(), d_loss.numpy(), d_loss_real.numpy(), d_loss_fake.numpy())
             )
-            
+
             d_probs_real = tf.reduce_mean(tf.sigmoid(d_logits_real))
             d_probs_fake = tf.reduce_mean(tf.sigmoid(d_logits_fake))
-            
-            epoch_d_vals.append((d_probs_real.numpy(),d_probs_fake.numpy()))
-    
-      print(
-          'Epoch {:03d} | ET {:.2f} min | Avg Losses G/D {:.4f}/{:.4f} [D-Real: {:.4f} D-Fake {:.4f}]'.format(
-              epoch, (time.time() - start_time)/60, *list(np.mean(epoch_losses, axis=0))
-          )
-      )
-    
-      ## Save model state ##
-      if epoch % checkpoint_frequency == 0:
-        gen_model.save_weights(gen_checkpoint_path.format(epoch=epoch))
-        disc_model.save_weights(disc_checkpoint_path.format(epoch=epoch))
+
+            epoch_d_vals.append((d_probs_real.numpy(), d_probs_fake.numpy()))
+
+        print(
+            'Epoch {:03d} | ET {:.2f} min | Avg Losses G/D {:.4f}/{:.4f} [D-Real: {:.4f} D-Fake {:.4f}]'.format(
+                epoch, (time.time() - start_time) / 60, *list(np.mean(epoch_losses, axis=0))
+            )
+        )
+
+        ## Save model state ##
+        if epoch % checkpoint_frequency == 0:
+            gen_model.save_weights(gen_checkpoint_path.format(epoch=epoch))
+            disc_model.save_weights(disc_checkpoint_path.format(epoch=epoch))
 
 
 if __name__ == '__main__':
@@ -241,10 +245,14 @@ if __name__ == '__main__':
     parser.add_argument('chps', type=int, help='Take checkpoint every x epochs')
     parser.add_argument('bSize', type=int, help='Batch Size to use')
     parser.add_argument('epochs', type=int, help='Number of epochs to train')
-    parser.add_argument('-c', '--checkpoints', type=str, dest="checkpoints", default="training", help="The output directory where the checkpoints are saved. It will be created if it dosen't exist and overritten (!) if it does.")
-    parser.add_argument('-d', '--data', type=str, dest="data", default="bilderNeuro", help="The directory containing subdirectories (labels) with images to use for training.")
-    parser.add_argument('-r', '--dropout', type=float, dest="dropout", default=0.2, help="The dropout rate to use for the discriminator")
-    parser.add_argument('-l', '--learnRate', type=float, dest="learnRate", default=0.001, help="The learning rate to use")
+    parser.add_argument('-c', '--checkpoints', type=str, dest="checkpoints", default="training",
+                        help="The output directory where the checkpoints are saved. It will be created if it dosen't exist and overritten (!) if it does.")
+    parser.add_argument('-d', '--data', type=str, dest="data", default="bilderNeuro",
+                        help="The directory containing subdirectories (labels) with images to use for training.")
+    parser.add_argument('-r', '--dropout', type=float, dest="dropout", default=0.2,
+                        help="The dropout rate to use for the discriminator")
+    parser.add_argument('-l', '--learnRate', type=float, dest="learnRate", default=0.001,
+                        help="The learning rate to use")
 
     args = parser.parse_args()
-    train_models(args.checkpoints, args.data, args.chps, args.bSize, args.epochs+1, args.dropout, args.learnRate)
+    train_models(args.checkpoints, args.data, args.chps, args.bSize, args.epochs + 1, args.dropout, args.learnRate)
